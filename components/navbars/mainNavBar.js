7f6bfae754abd6bfa376363fb288769c71c9cccf
mario <carcotamario5@gmail.com>
2024-07-30 23:00:29 +0200

    Fixed navbar redirection

--- components/navbars/mainNavBar.js
+++ components/navbars/mainNavBar.js
@@ -22,22 +22,22 @@ const MainNavBar = () => {
 
 const Logo = () => {
   return (
-    <div className="flex justify-center items-center gap-1">
-      <BookDashed stroke="#EC4E20" size={30} />
-      <h1 className="md:block hidden text-2xl text-tblack font-bold">
-        Mario&apos;s Blog
-      </h1>
-    </div>
+    <TransitionLink href={"/"}>
+      <div className="flex justify-center items-center gap-1">
+        <BookDashed stroke="#EC4E20" size={30} />
+        <h1 className="md:block hidden text-2xl text-tblack font-bold">
+          Mario&apos;s Blog
+        </h1>
+      </div>
+    </TransitionLink>
   );
 };
 
 const NavLeft = () => {
   return (
-    <TransitionLink href={"/"}>
-      <div className="flex items-center gap-6">
-        <Logo />
-      </div>
-    </TransitionLink>
+    <div className="flex items-center gap-6">
+      <Logo />
+    </div>
   );
 };
 
